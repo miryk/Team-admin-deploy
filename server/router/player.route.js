89e1de7fc4283ba549c6ca0f2c7868c8d9d19654
Miry Konishi <miry.konishi@gmail.com>
2023-02-25 00:34:51 -0300

    Update API

--- server/router/player.route.js
+++ server/router/player.route.js
@@ -1,10 +1,10 @@
 const PlayerController = require("../controller/player.controller");
 
 module.exports = (app) => {
-  app.get("api/test", PlayerController.test);
-  app.get("api/", PlayerController.getAllPlayers);
-  app.post("api/", PlayerController.addPlayer);
-  app.get("api/:id", PlayerController.getPlayerById);
-  app.delete("api/:id", PlayerController.removePlayer);
-  app.put("api/:id", PlayerController.updatePlayer);
+  app.get("/api/test", PlayerController.test);
+  app.get("/api/", PlayerController.getAllPlayers);
+  app.post("/api/", PlayerController.addPlayer);
+  app.get("/api/:id", PlayerController.getPlayerById);
+  app.delete("/api/:id", PlayerController.removePlayer);
+  app.put("/api/:id", PlayerController.updatePlayer);
 };
